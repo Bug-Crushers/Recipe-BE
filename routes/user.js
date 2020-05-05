396702a5e54052ddf5a774a48c4bf5ca2acd6a28
Leelsmuth <femianjorin@rocketmail.com>
2020-05-05 18:41:44 +0100

    checking travis

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-// const passport = require('passport');
 const router = express.Router();
 
 router.get('/', (req, res) => {
