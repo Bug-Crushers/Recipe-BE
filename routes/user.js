82e121870dcc5071fba7695d886785f14078684e
Leelsmuth <femianjorin@rocketmail.com>
2020-05-05 18:29:21 +0100

    initial setup for CI

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const passport = require('passport');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ api: 'family secret' });
+  // res.json('family secret')
+});
+
+
+module.exports = router;
