deaaf93fdad999ddd83ddbb542460d47e851ee8e
Leelsmuth <femianjorin@rocketmail.com>
2020-05-05 18:28:43 +0100

    connected to mongodb and update gitignore

--- config/database.js
+++ config/database.js
@@ -1,5 +1,11 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = { mongoURI: 'mongodb+srv://anjorinio:1234@cluster0-hogdg.gcp.mongodb.net/test?retryWrites=true&w=majority' }
-} else {
-  module.exports = { mongoURI: 'mongodb://localhost/family-recipe' }
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  console.log(`MongoDB connected: ${conn.connection.host}`);
 }
+
+module.exports = connectDB;
