aeb27139b949256bb37ae2ed089c4309b0e28a53
Leelsmuth <femianjorin@rocketmail.com>
2020-05-05 18:26:26 +0100

    moved server.js to index.js

--- api/server.spec.js
+++
@@ -1,9 +0,0 @@
-const expect = require('supertest');
-const server = require('./server');
-
-describe('test', () => {
-  test('shoult return 200 OK', async () => {
-    const response = await expect(server).get('/')
-    expect(response.status).toBe(200)
-  })
-});
